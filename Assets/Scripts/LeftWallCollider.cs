a7a5263a018356a72189bc16f038357b221fc2e6
Andrew Lohmann <lohmann333@gmail.com>
2016-05-15 15:47:42 -0400

    Added the DirectionalLookingScene, which uses the Cardboard prefab to test where the player is looking. This will need to be integrated into the project to use Oculus instead of Cardboard.

---
+++ Assets/Scripts/LeftWallCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftWallCollider : MonoBehaviour {
+
+	void OnTriggerEnter(Collider otherCollider)
+	{
+		if(otherCollider.gameObject.name == "PlayerRightCollider")
+		{
+			transform.parent.GetComponent<WallManager> ().SetBeingLookedAt (false);
+		}
+	}
+}
---
+++ Assets/Scripts/RightWallCollider.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightWallCollider : MonoBehaviour {
+
+	void OnTriggerEnter(Collider otherCollider)
+	{
+		if(otherCollider.gameObject.name == "PlayerLeftCollider")
+		{
+			transform.parent.GetComponent<WallManager> ().SetBeingLookedAt (false);
+		}
+	}
+}
