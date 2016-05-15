51b6df65a4b54a6dcda042fde69b90a88863093f
hhassan1230 <hhassan@oswego.edu>
2016-05-15 14:43:42 -0400

    puts in working texture change

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManagerScript : MonoBehaviour {
+	public GameObject frame;
+	public Material nextPicture;
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(CallChangeColor(nextPicture));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	IEnumerator CallChangeColor(Material pic)
+	{
+		yield return new WaitForSeconds(2);		
+		ChangeFrameColor(pic);
+	}
+
+	private void ChangeFrameColor(Material thePic)
+	{
+		ShadeChanger scriptChanger = (ShadeChanger) frame.GetComponent(typeof(ShadeChanger));
+		scriptChanger.ChangeColor(thePic);
+	}
+}
---
+++ Assets/ShadeChanger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShadeChanger : MonoBehaviour {
+	public Material newMaterial;
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("I'm working");
+//		ChangeColor (newMaterial);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ChangeColor(Material currentMaterial)
+	{
+		GetComponent<Renderer>().material = currentMaterial;
+
+	}
+}
