9ccc608915b2535d3aa50281b78946679f75a8bd
hhassan1230 <hhassan@oswego.edu>
2016-05-20 02:53:37 -0400

    creates working blink logic

--- Assets/ClickTest.cs
+++ Assets/ClickTest.cs
@@ -6,13 +6,14 @@ public class ClickTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(Input.GetButtonDown("Fire1")){
 			GameManagerScript scriptChanger = (GameManagerScript) script.GetComponent(typeof(GameManagerScript));
+
 			scriptChanger.changePicture();
 		}
 	
