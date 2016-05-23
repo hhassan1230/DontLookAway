e7064185c200678bb6c212bbd77427b910d9e995
Andrew Lohmann <lohmann333@gmail.com>
2016-05-22 20:45:15 -0400

    Added a scene called LightningTest which has lightning effect and candle fade in/out

--- Assets/GameManagerScript.cs
+++ Assets/GameManagerScript.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class GameManagerScript : MonoBehaviour {
 	public GameObject frame;
+	public Material currentPicture;
 	public Material nextPicture;
 	public Material[] pics;
 	public GameObject moster;
@@ -13,23 +14,37 @@ public class GameManagerScript : MonoBehaviour {
 	void Start () {
 //		StartCoroutine(CallChangeColor(nextPicture));
 		count = 0;
-	}
-	
-	void Update () {
-	
+		LightningManager.lightningDelegate += FlickerToFifthState;
 	}
 
 	public void changePicture(){
-		if(count < 4){
-			Material texture = pics [count];
-			ChangeFrameColor (texture);
+		if(count < (pics.Length - 1)){
+			currentPicture = pics [count];
+			ChangeFrameColor (currentPicture);
 			count++;
 		}
-		if (count == 4) {
+		if (count == (pics.Length - 1)) {
 			Instantiate (moster, new Vector3 (-2.73f, 1f, -0.1f), Quaternion.identity);
 			count++;
 		} 
 
+	}
+
+	public void FlickerToFifthState(bool newState)
+	{
+		//this function is intended to flicker the image back between the current texture and the last
+		if(newState == true)
+		{
+			currentPicture = pics [count];
+			ChangeFrameColor (pics[pics.Length - 1]);
+		}
+		else
+		{
+			ChangeFrameColor (currentPicture);
+			//uncomment this next line if you wat the picture to flicker each time the lightning flashes,
+			//instead of just the first time
+			LightningManager.lightningDelegate -= FlickerToFifthState;
+		}
 	}
 
 	IEnumerator CallChangeColor(Material pic)
@@ -42,5 +57,6 @@ public class GameManagerScript : MonoBehaviour {
 	{
 		ShadeChanger scriptChanger = (ShadeChanger) frame.GetComponent(typeof(ShadeChanger));
 		scriptChanger.ChangeColor(thePic);
+
 	}
 }
