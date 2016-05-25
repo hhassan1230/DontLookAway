cdc8de370f03acf548882db6ef492cf0e6d74434
hhassan1230 <hhassan@oswego.edu>
2016-05-25 18:50:41 -0400

    adds blick with function call

---
+++ Assets/EyeLidManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EyeLidManager : MonoBehaviour {
+	private 
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LidClose(){
+		print ("Hi Pal!");
+	}
+}
---
+++ Assets/PhoneManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PhoneManager : MonoBehaviour {
+	private GameObject phone;
+	private GameObject player;
+	private AudioSource phoneMessage;
+	// Use this for initialization
+	void Start () {
+		phone = GameObject.FindGameObjectWithTag ("Phone");
+		player = GameObject.FindGameObjectWithTag ("MainCamera");
+		phoneMessage = phone.GetComponent<AudioSource>();
+	}
+
+	public void Play911(){
+		phoneMessage.Play ();
+	}
+}
